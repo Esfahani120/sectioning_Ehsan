c28f8cb63bb267529e4b88e712a92190b279e747
Balaji Pokuri <balajip@iastate.edu>
2020-02-14 16:37:14 -0600

    [minor] better variable naming

--- sectioning_Ehsan.py
+++ sectioning_Ehsan.py
@@ -27,11 +27,10 @@ morphologies which obtained by solving the Cahn-Hilliard equation.
 
 import os
 from tqdm import tqdm
-import h5py  
+import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 """ Slicing core function 
    * This function takes a pandas.core.frame.DataFrame type dataframe (df0)
@@ -39,14 +38,17 @@ import matplotlib.pyplot as plt
    * df0 contains node coordinates and phi values (approximately for each 
      morphology or timestep, df0.shape = (8520321, 4))
 """
+
+
 def slicing(df0):
-    """grouping the data frame (df0) by X values""" 
-    df_sec_X=df0.groupby('X').apply(pd.DataFrame.to_numpy) 
-    
+    """grouping the data frame (df0) by X values"""
+    df_sec_X = df0.groupby('X').apply(pd.DataFrame.to_numpy)
+
     """grouping the data frame (df0) by Y values"""
-    df_sec_Y=df0.groupby('Y').apply(pd.DataFrame.to_numpy)
-    
+    df_sec_Y = df0.groupby('Y').apply(pd.DataFrame.to_numpy)
+
     return df_sec_X, df_sec_Y
+
 
 """ Setting directory, and reading names of the files:
     * Note: to avoide any unpredicted errors, please make two seprate 
@@ -60,7 +62,7 @@ name_files = os.listdir(path=reading_directory)
 """ Geometry parameters:
     * width : number of nodes along width (X, Y-dirs)
     * height : number of nodes along height (Z-dir)
-    * numb_sections : number of the slices you need in both X-dir and Y-dir 
+    * num_sections : number of the slices you need in both X-dir and Y-dir 
       (you can change it if you want to change the distance between the 
       slices, e.g. we have one slice every 5 steps)
     * sec_loc: by knowing that the dimension of the sample is 
@@ -70,10 +72,10 @@ name_files = os.listdir(path=reading_directory)
       from 257x129 images, we need to define patch_1 and patch_2 in which we 
       can find the indices we need to this end. 
 """
-width = 257 
+width = 257
 height = 129
 
-sec_loc = np.linspace(0.0, 4.0, width) 
+sec_loc = np.linspace(0.0, 4.0, width)
 
 numb_sections = len(sec_loc[np.arange(0, width, 5)])
 
@@ -93,68 +95,67 @@ patch_2 = patch_1 + height - 1
 """
 
 """ Iterating over names of the files (*.h5) in the reading_directory
-""" 
+"""
 writing_data = h5py.File(writing_directory + 'sliced_dataset.h5', 'w')
 k = 1
 for names in name_files:
 
-    writing_group = writing_data.create_group('file_'+str(k-1))
+    writing_group = writing_data.create_group('file_' + str(k - 1))
     reading_data = h5py.File(reading_directory + names, 'r')
-    
+
     keys_list = list(reading_data.keys())
     time_interval = np.arange(8, len(keys_list), 2)
 
     """ Iterating over the time_interval defined by the user
     """
-    for timestep in tqdm(time_interval): 
+    for timestep in tqdm(time_interval):
 
-        saving_frames = np.zeros((numb_sections*4, height*height))
-        
+        saving_frames = np.zeros((numb_sections * 4, height * height))
+
         """ Reading the data set related to the current 
             timestep in the loop
         """
-        dset = reading_data[keys_list[timestep]] 
-        
+        dset = reading_data[keys_list[timestep]]
+
         """ Extracting 8520321 (257x257x129) nodes with 3 
             (xyz) coordinates from the read_dset 
         """
-        ncoord = np.array(dset.get('node_coords')) 
-        
+        ncoord = np.array(dset.get('node_coords'))
+
         """ Extracting values of phi on 8520321 (257x257x129) nodes
-        """ 
-        phi_values = np.array(dset.get('node_data').get('phi')) 
-        
+        """
+        phi_values = np.array(dset.get('node_data').get('phi'))
+
         """ Concatenation of ncoord and phi_values in a pandas 
             dataframe and name the columns properly
         """
-        df = pd.concat([pd.DataFrame(ncoord,columns=['X','Y','Z']),
-                        pd.DataFrame(phi_values,columns=['phi'])], 
-                        axis=1, sort=False)
-        
+        df = pd.concat([pd.DataFrame(ncoord, columns=['X', 'Y', 'Z']),
+                        pd.DataFrame(phi_values, columns=['phi'])],
+                       axis=1, sort=False)
+
         """ Calling slicing function to groupby the dataset 
             based on the X and Y coordinates 
         """
         data_sliced = slicing(df)
         slices_x = data_sliced[0]
         slices_y = data_sliced[1]
-        
+
         """ Dividing the images obtained from the slicing functions 
             in half by using indices saved in patch_1 and patch_2 arrays 
-        """ 
-        for j, i in enumerate(range(0, numb_sections*4, 4)):
-            saving_frames[i] = slices_x[sec_loc[j]][:,3][patch_1]
-            saving_frames[i+1] = slices_x[sec_loc[j]][:,3][patch_2]
-            saving_frames[i+2] = slices_y[sec_loc[j]][:,3][patch_1]    
-            saving_frames[i+3] = slices_y[sec_loc[j]][:,3][patch_2]  
-            
+        """
+        for j, i in enumerate(range(0, numb_sections * 4, 4)):
+            saving_frames[i] = slices_x[sec_loc[j]][:, 3][patch_1]
+            saving_frames[i + 1] = slices_x[sec_loc[j]][:, 3][patch_2]
+            saving_frames[i + 2] = slices_y[sec_loc[j]][:, 3][patch_1]
+            saving_frames[i + 3] = slices_y[sec_loc[j]][:, 3][patch_2]
+
         """ Saving the saving_frames in which one can find all the desired
             129x129 size images. saving_frames size is: 
-            (numb_sections*4, height*height)
+            (num_sections*4, height*height)
         """
         writing_group.create_dataset(str(timestep), data=saving_frames)
     print('Slicing files: {} out of {} ---> Done'.format(k, len(name_files)))
     k += 1
-         
 
     reading_data.close()
 writing_data.close()
@@ -170,8 +171,8 @@ fig, axes = plt.subplots(2, 2)
 k = 0
 for i in range(2):
     for j in range(2):
-        axes[i, j].imshow(saving_frames[k].reshape(129,129), extent=[0, 2, 0, 2], origin='lower', cmap='jet')
-        axes[i, j].set_title("patch_" + str(j+1))
+        axes[i, j].imshow(saving_frames[k].reshape(129, 129), extent=[0, 2, 0, 2], origin='lower', cmap='jet')
+        axes[i, j].set_title("patch_" + str(j + 1))
         axes[i, j].axis('off')
         k += 1
 plt.show()
